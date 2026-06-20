c6e081f6ea2992cd915f5b5d389b341334fce2a5
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2026-06-20 15:25:01 +0200

    Update evolution_chains.py

--- pypokeclient/_api/evolution/evolution_chains.py
+++ pypokeclient/_api/evolution/evolution_chains.py
@@ -35,6 +35,7 @@ class EvolutionDetail:
     min_happiness: int | None
     min_beauty: int | None
     min_affection: int | None
+    near_special_rock: bool
     needs_multiplayer: bool
     needs_overworld_rain: bool
     party_species: NamedAPIResource | None
@@ -43,8 +44,8 @@ class EvolutionDetail:
     time_of_day: str
     trade_species: NamedAPIResource | None
     turn_upside_down: bool
-    region_id: NamedAPIResource | None
-    base_form_id: NamedAPIResource | None
+    region: NamedAPIResource | None
+    base_form: NamedAPIResource | None
     used_move: NamedAPIResource | None
     min_move_count: int | None
     min_steps: int | None
